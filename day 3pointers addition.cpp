0af58c68fed066e689158eafa5c5984165b96e20
Lohitha K S <112691302+Lohitha192110507@users.noreply.github.com>
2022-09-13 11:23:19 +0530

    Add files via upload

---
+++ day 3pointers addition.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+   int first, second, *p, *q, sum,j=1;
+    while(j==1)
+    {
+	
+    printf("Enter two integers to add\n");
+    scanf("%d%d", &first, &second);
+
+    p = &first;
+    q = &second;
+
+    sum = *p + *q;
+
+    printf("Sum of the numbers = %d\n", sum);
+    scanf("%d",&j);
+    }
+
+}
